b7d0d2bdeda6122555b119bbcf99defed2607be4
lei.song <moon.song@yeahmobi.com>
2020-07-31 19:00:26 +0800

    docs: vue 初始化-实例成员

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -5,19 +5,25 @@ import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 
-function Vue (options) {
-  if (process.env.NODE_ENV !== 'production' &&
-    !(this instanceof Vue)
-  ) {
-    warn('Vue is a constructor and should be called with the `new` keyword')
-  }
-  this._init(options)
+function Vue(options) {
+    if (process.env.NODE_ENV !== 'production' && !(this instanceof Vue)) {
+        warn('Vue is a constructor and should be called with the `new` keyword')
+    }
+    this._init(options)
 }
 
+// 注册 vm 的 init() 方法，初始化 vm
 initMixin(Vue)
+// 注册 vm 的 $data/$props/$set/$delete/$watch
 stateMixin(Vue)
+// 初始化事件相关
+// $on/$once/$off/$emit
 eventsMixin(Vue)
+// 初始化生命周期相关的混入方法
+// _update/$forceUpdate/$destory
 lifecycleMixin(Vue)
+// 混入 render
+// $nextTick/_render
 renderMixin(Vue)
 
 export default Vue

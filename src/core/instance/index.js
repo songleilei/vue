d6d3b8b4c8d7ea7561f992636dd8a450691ca682
rf y <709241197@qq.com>
2020-08-01 11:07:23 +0800

    docs: vue instance index.js

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -5,11 +5,12 @@ import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 
+// 此处不用 class 的原因是因为方便，后续给 Vue 实例混入实例成员
 function Vue(options) {
-    if (process.env.NODE_ENV !== 'production' && !(this instanceof Vue)) {
-        warn('Vue is a constructor and should be called with the `new` keyword')
-    }
-    this._init(options)
+  if (process.env.NODE_ENV !== 'production' && !(this instanceof Vue)) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)
 }
 
 // 注册 vm 的 init() 方法，初始化 vm
